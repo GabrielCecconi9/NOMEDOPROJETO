704c68f41be55b80de68820daaacb9a3f290d1ea
GABRIEL GUIMARÃES CECCONI <gabriel_cecconi@estudante.sesisenai.org.br>
2023-09-13 12:13:33 -0300

    aaa

--- App.js
+++ App.js
@@ -1,20 +1,118 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
-export default function App() {
+const App = () => {
+  const [nome, setNome] = useState('');
+  const [genero, setGenero] = useState('');
+  const [dataNascimento, setDataNascimento] = useState('');
+  const [usuario, setUsuario] = useState('');
+  const [senha, setSenha] = useState('');
+  const [email, setEmail] = useState('');
+  const [confirmarEmail, setConfirmarEmail] = useState('');
+  const [cpf, setCpf] = useState('');
+  const [idioma, setIdioma] = useState('');
+
+  const handleCadastro = () => {
+    console.log('Valores informados:');
+    console.log('Nome:', nome);
+    console.log('Gênero:', genero);
+    console.log('Data de Nascimento:', dataNascimento);
+    console.log('Usuário:', usuario);
+    console.log('Senha:', senha);
+    console.log('E-mail:', email);
+    console.log('Confirmar E-mail:', confirmarEmail);
+    console.log('CPF:', cpf);
+    console.log('Idioma do Currículo:', idioma);
+  };
+
   return (
+    
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.titulo}>
+      <text> Tela de cadastro</text>
+    </View>
+      <TextInput
+        style={styles.input}
+        placeholder="Nome Completo"
+        value={nome}
+        onChangeText={setNome}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Gênero"
+        value={genero}
+        onChangeText={setGenero}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Data de Nascimento"
+        value={dataNascimento}
+        onChangeText={setDataNascimento}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Usuário"
+        value={usuario}
+        onChangeText={setUsuario}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Senha"
+        secureTextEntry
+        value={senha}
+        onChangeText={setSenha}
+      />
+      <TextInput  
+        style={styles.input}
+        placeholder="E-mail"
+        value={email}
+        onChangeText={setEmail}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Confirme seu E-mail"
+        value={confirmarEmail}
+        onChangeText={setConfirmarEmail}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="CPF"
+        value={cpf}
+        onChangeText={setCpf}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Idioma do Currículo"
+        value={idioma}
+        onChangeText={setIdioma}
+      />
+      <Button title="CADASTRAR" onPress={handleCadastro} />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    backgroundColor: 'gray',
     justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+  },
+  input: {
+    width: '30%',
+    height: 40,
+    borderColor: 'black',
+    borderWidth: 2,
+    marginBottom: 16,
+    paddingLeft: 8,
+    backgroundColor: 'white',
+  },
+  titulo:{
+    fontSize: 60,
+    marginBottom: 40,
   },
 });
+
+export default App;
